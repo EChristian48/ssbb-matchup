248fc2e464c555a31dbee63388fe9d28a0a32efc
Erick Christian <erickchristian48@gmail.com>
2021-08-05 19:58:57 +0700

    refactor: make title separate component

---
+++ components/Title.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="mt-2">
+    <h1 class="title1">SUPER SMASH BROS.</h1>
+    <h1 class="title2">BRAWL</h1>
+    <h1 class="title3">MATCHUP</h1>
+  </v-card>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap');
+
+.title1,
+.title2,
+.title3 {
+  font-family: 'Roboto Condensed', 'Open Sans', sans-serif;
+  font-weight: 700;
+  margin: 0;
+  text-align: right;
+}
+
+.title2 {
+  font-size: 7rem;
+  margin-top: -50px;
+}
+
+.title3 {
+  margin-top: -50px;
+}
+</style>
