91079bbe313da9cae93c6f71127463b737be1813
Erick Christian <erickchristian48@gmail.com>
2021-08-06 13:33:58 +0700

    polished error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,44 +1,13 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <v-main>
+      <v-container>
+        <v-card class="text-center py-4">
+          <h1>Error</h1>
+          <v-btn nuxt to="/">Back to Home Page</v-btn>
+        </v-card>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  data () {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred'
-    }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
-}
-</script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
