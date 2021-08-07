06917d4c43327687127e0476055915b926fea9c7
Erick Christian <erickchristian48@gmail.com>
2021-08-07 19:59:55 +0700

    add brawl fighter list page

---
+++ pages/brawl/fighters.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card>
+    <h1>Fighter</h1>
+
+    <ul>
+      <li v-for="fighterDocument in fighterDocuments" :key="fighterDocument.id">
+        {{ fighterDocument.title }}
+      </li>
+    </ul>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { IContentDocument } from '@nuxt/content/types/content'
+import Vue from 'vue'
+
+type FighterListData = {
+  fighterDocuments: IContentDocument | IContentDocument[]
+}
+
+export default Vue.extend({
+  async asyncData({ $content }): Promise<FighterListData> {
+    const fighterDocuments = await $content('brawl/fighters').fetch()
+
+    return {
+      fighterDocuments
+    }
+  },
+  data(): FighterListData {
+    return {} as FighterListData
+  }
+})
+</script>
