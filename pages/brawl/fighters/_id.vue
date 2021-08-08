8908213d05995f3cd3ae7c5aef99b73f6db56b44
Erick Christian <erickchristian48@gmail.com>
2021-08-08 11:25:03 +0700

    done getting params

---
+++ pages/brawl/fighters/_id.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>{{ id }}</v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type FighterData = {
+  id: string
+}
+
+export default Vue.extend({
+  asyncData({ params: { id } }): FighterData {
+    return {
+      id
+    }
+  },
+  data(): FighterData {
+    return {} as FighterData
+  }
+})
+</script>
