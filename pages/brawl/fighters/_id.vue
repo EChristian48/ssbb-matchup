9f45f2fc2f785536d94363ecb43d1309e63c6880
Erick Christian <erickchristian48@gmail.com>
2021-08-08 11:52:32 +0700

    done individual fighter page

--- pages/brawl/fighters/_id.vue
+++ pages/brawl/fighters/_id.vue
@@ -1,22 +1,42 @@
 <template>
-  <v-card>{{ id }}</v-card>
+  <v-container>
+    <v-card>
+      <v-img :src="fighterDocument.imageUrl"></v-img>
+      <v-card-title>{{ fighterDocument.title }}</v-card-title>
+      <v-card-subtitle>Fighter #{{ fighterDocument.id }}</v-card-subtitle>
+    </v-card>
+
+    <v-card class="mt-3">
+      <v-card-text>
+        <nuxt-content :document="fighterDocument"></nuxt-content>
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
+import { IContentDocument } from '@nuxt/content/types/content'
 import Vue from 'vue'
 
 type FighterData = {
-  id: string
+  fighterDocument: IContentDocument
 }
 
 export default Vue.extend({
-  asyncData({ params: { id } }): FighterData {
+  async asyncData({ params: { id }, $content }): Promise<FighterData> {
+    const fighterDocument = (await $content('/brawl/fighters')
+      .where({ id: parseInt(id) })
+      .fetch()) as IContentDocument[]
+
     return {
-      id
+      fighterDocument: fighterDocument[0]
     }
   },
   data(): FighterData {
     return {} as FighterData
+  },
+  created() {
+    console.log(this.fighterDocument)
   }
 })
 </script>
